5186b248d29eaf63bf4cf1dccff0e71d30f433b0
Koe <tonphan495@gmail.com>
2026-03-11 13:50:26 +0700

    b1

---
+++ thuchanh1/thuchanh1/src/main/java/com/example/thuchanh1B4/B4_BMICalculator.java
@@ -0,0 +1,12 @@
+package com.example.thuchanh1B4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class B4_BMICalculator {
+	public static void main(String[] args) {
+		SpringApplication.run(B4_BMICalculator.class, args);
+	}
+}
