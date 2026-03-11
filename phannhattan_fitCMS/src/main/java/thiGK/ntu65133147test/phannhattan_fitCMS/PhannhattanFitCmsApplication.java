02c9008ba6bf766bc063b6d22a2ed3c79c4c37dd
Koe <tonphan495@gmail.com>
2026-03-11 14:39:45 +0700

    test giữa kì

---
+++ phannhattan_fitCMS/src/main/java/thiGK/ntu65133147test/phannhattan_fitCMS/PhannhattanFitCmsApplication.java
@@ -0,0 +1,13 @@
+package thiGK.ntu65133147test.phannhattan_fitCMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhannhattanFitCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhannhattanFitCmsApplication.class, args);
+	}
+
+}
---
+++ phannhattan_fitCMS/src/test/java/thiGK/ntu65133147test/phannhattan_fitCMS/PhannhattanFitCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package thiGK.ntu65133147test.phannhattan_fitCMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhannhattanFitCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
