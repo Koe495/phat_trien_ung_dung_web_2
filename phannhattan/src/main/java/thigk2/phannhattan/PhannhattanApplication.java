02b391aa4e80249e98e328d76575015ae282a5ac
Koe <tonphan495@gmail.com>
2026-04-24 13:54:37 +0700

    Lần thứ 1

---
+++ phannhattan/src/main/java/thigk2/phannhattan/PhannhattanApplication.java
@@ -0,0 +1,13 @@
+package thigk2.phannhattan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhannhattanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhannhattanApplication.class, args);
+	}
+
+}
---
+++ phannhattan/src/test/java/thigk2/phannhattan/PhannhattanApplicationTests.java
@@ -0,0 +1,13 @@
+package thigk2.phannhattan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhannhattanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
